9d9585c97ed00eb9b1347a001c73d9aff613b45f
JosephBaquerizo <61207703+JosephBaquerizo@users.noreply.github.com>
2022-09-30 13:51:02 -0500

    react ahora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Alert from './components/alert/alert';
+import Header from './layouts/Header';
+import RoutesComponent from './routes/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert />
+      <Header />
+      <RoutesComponent />
+    </>
   );
 }
 
